5d574515aaf32862463c68caa4eaecade14cac89
Jim9025 <jim0988845382@gmail.com>
2025-11-10 13:42:08 +0800

    Create Sample3_1.java

---
+++ Sample3_1.java
@@ -0,0 +1,10 @@
+public class Sample3_1 {
+    public static void main(String[] args) {
+        Car car1;           // 宣告 car1 為 Car 類別的變數
+        car1 = new Car();   // 建立 Car 物件並指定給變數 car1
+    }
+}
+
+class Car {
+    // Car 類別的定義（目前是空的）
+}
